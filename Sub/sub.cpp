3ce5c4d19b4fd91316905ea68650b0df84aa4758
Your Name <you@example.com>
2017-05-21 13:51:32 +0900

    Add Sample Main, Sub, and Doxygen

---
+++ Sub/sub.cpp
@@ -0,0 +1,6 @@
+#include "sub.h"
+#include <iostream>
+
+void Func(void) {
+    std::cout << "Hello!" << std::endl;
+}
---
+++ include/sub.h
@@ -0,0 +1,6 @@
+#ifndef SUB_H
+#define SUB_H
+
+void Func(void);
+
+#endif // SUB_H
